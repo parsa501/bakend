b33c1ed12c9ba9fc036c968f9a45a60f21dda811
parsa501 <safazadehparsa@gmail.com>
2025-09-06 22:37:13 -0500

    add bakend chat

---
+++ Models/UserMd.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:[true,"Username is required"],
+        unique:[true,"Username must be unique"]
+    },
+    password:{
+        type:String,
+        required:[true,"Password is required"],
+    },
+    role:{
+        type:String,
+        enum:["user","admin"],
+        default:"user"
+    }
+}, {timestamps:true})
+
+const User = mongoose.model("User", userSchema);
+export default User;
